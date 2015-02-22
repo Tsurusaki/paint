e81890a9bc5e1f1542530a278a649d1bc2acca8e
Tsurusaki <ka.trsk@gmail.com>
2015-02-22 17:28:01 +0900

    Initial Commit

---
+++ techpaint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  techpaint
+//
+//  Created by 鶴崎かんな on 2015/02/22.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
